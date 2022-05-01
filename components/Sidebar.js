8df482e4659225edcae04982c093815d77b66017
AayushMohan <aayushmohan1702@gmail.com>
2022-05-01 19:44:20 +0530

    Added Connect Button

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -55,6 +55,10 @@ const Sidebar = () => {
             )}
           </>
         )}
+
+        <div className={styles.connectButton}>
+          <ConnectButton />
+        </div>
       </div>
     </div>
   );
