fddce381e018402ef868234b875ceed2e407a56f
AayushMohan <aayushmohan1702@gmail.com>
2022-05-02 21:32:51 +0530

    Added Styles in cards

--- components/Cards.js
+++ components/Cards.js
@@ -3,6 +3,12 @@ import { AmazonContext } from "../context/AmazonContext";
 import Card from "./Card";
 
 const Cards = () => {
+  const styles = {
+    container: `h-full w-full flex flex-col ml-[20px] mt-[50px]`,
+    title: `text-xl font-bolder mb-[20px] mt-[30px] ml-[30px]`,
+    cards: `flex items-center flex-wrap gap-[80px]`,
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>New Release</div>
