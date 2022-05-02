9b1bcd3fa7308f6a6e28798d377a5f4a87ca88b5
AayushMohan <aayushmohan1702@gmail.com>
2022-05-02 21:39:43 +0530

    Added Items, attributes

--- components/Cards.js
+++ components/Cards.js
@@ -3,6 +3,14 @@ import { AmazonContext } from "../context/AmazonContext";
 import Card from "./Card";
 
 const Cards = () => {
+  const items = {
+    id: 0,
+    attributes: {
+      name: "Doge",
+      price: 2,
+      src: "https://media1.giphy.com/media/oBQZIgNobc7ewVWvCd/giphy.gif?cid=ecf05e47y86zf0a44okj0fksvb83wc3n7x1xpkvtva6nbqg4&rid=giphy.gif&ct=g",
+    },
+  };
   const styles = {
     container: `h-full w-full flex flex-col ml-[20px] mt-[50px]`,
     title: `text-xl font-bolder mb-[20px] mt-[30px] ml-[30px]`,
@@ -13,7 +21,7 @@ const Cards = () => {
     <div className={styles.container}>
       <div className={styles.title}>New Release</div>
       <div className={styles.cards}>
-        <Card />
+        <Card key={items.id} item={items.attributes} />
       </div>
     </div>
   );
