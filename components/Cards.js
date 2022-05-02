49e96a381d62252a3a7484cc59cbf391843fa6cc
AayushMohan <aayushmohan1702@gmail.com>
2022-05-02 21:28:09 +0530

    Added Card.js

--- components/Cards.js
+++ components/Cards.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { AmazonContext } from "../context/AmazonContext";
+import Card from "./Card";
 
 const Cards = () => {
-  return <div>Cards</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>New Release</div>
+      <div className={styles.cards}>
+        <Card />
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
