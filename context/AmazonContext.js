24f333291464ed26011d6c00940723d3c87ca07e
AayushMohan <aayushmohan1702@gmail.com>
2022-05-02 20:32:56 +0530

    Added useEffect

--- context/AmazonContext.js
+++ context/AmazonContext.js
@@ -7,5 +7,22 @@ export const AmazonProvider = ({ children }) => {
   const [username, setUsername] = useState("");
   const [nickname, setNickname] = useState("");
 
+  const {
+    authenticated,
+    isAuthenticated,
+    enableWeb3,
+    Moralis,
+    user,
+    isWeb3Enabled,
+  } = useMoralis;
+
+  useEffect(() => {
+    (async () => {
+      if (isAuthenticated) {
+        const currentUsername = await user?.get("nickname");
+      }
+    })();
+  }, [isAuthenticated, user, username]);
+
   return <AmazonContext.Provider value={{}}>{children}</AmazonContext.Provider>;
 };
